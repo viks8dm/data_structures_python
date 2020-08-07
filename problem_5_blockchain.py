179b09a464848679fe2f71bcc86425dc9e5bbc2c
Vikalp <vikalp.mishra@gmail.com>
2020-08-07 11:49:51 -0700

    Update problem_5_blockchain.py

--- problem_5_blockchain.py
+++ problem_5_blockchain.py
@@ -5,11 +5,11 @@ import hashlib
 import datetime
 
 ######################
-class DoubleNode:
+class Node:
     def __init__(self, data, previous_hash=None):
         self.block = Block(datetime.datetime.utcnow(), data, previous_hash)
         self.next = None
-        self.prev = None
+        # self.prev = None
 
 ######################
 class Block:
@@ -42,20 +42,21 @@ class Blockchain:
         self.head = None
         self.tail = None
 
-    def append(self, data):
+    def append(self, data=None):
+        if data is None:
+            print('block provided is empty: nothing to append')
+            return
+
         if data:
             if self.head is None:
-                self.head = DoubleNode(data)
+                self.head = Node(data)
                 self.tail = self.head
             else:
-                self.tail.next = DoubleNode(data, self.tail.block.hash)
-                self.tail.next.prev = self.tail
+                self.tail.next = Node(data, self.tail.block.hash)
                 self.tail = self.tail.next
-        else:
-            print("ERROR: block is empty")
 
         return
-    
+   
     def __str__(self):
         if self.head is None:
             return "Block chain is empty"
@@ -69,12 +70,33 @@ class Blockchain:
 
 ######################
 if __name__=="__main__":
-    ####### test #########
-    testblock = Blockchain()
-    print('------------- adding blocks ----------')
-    testblock.append("b-1")
-    testblock.append("b-2")
-    testblock.append("b-3")
+    # test -1 : empty blockchain
+    print('================================================')
+    print('------ test - 1: empty blockchain case')
+    b1 = Blockchain()
+    print(b1)   # OUTPUT: Block chain is empty
 
-    print(testblock)
-    print ('------ DONE ---------')
+    # test - 2: empty appends
+    print('================================================')
+    print('----- test - 2: empty appends, reduced chain size')
+    b1.append() # OUTPUT: block provided is empty: nothing to append
+    b1.append()   # OUTPUT: block provided is empty: nothing to append
+    b1.append('new-b')
+    b1.append()   # OUTPUT: block provided is empty: nothing to append
+    print(b1)   # prints just one block for "new-b"
+
+    #-------- test - 3: set of valid values printed in sequence
+    print('================================================')
+    print('---------- test - 3: print all or specified blocks in the blockchain')
+    t1 = Blockchain()
+    t1.append("b-1")
+    t1.append("b-2")
+    t1.append("b-3")
+    print('-- print all ')
+    print(t1)
+
+    print('-- print specific')
+    print(t1.head.block.previous_hash)  # OUTPUT: None
+    print(t1.head.block.timestamp)  # OUTPUT: timestamp value for 1st element
+    print(t1.head.block.hash)  # OUTPUT: hash value for 1st element
+    print(t1.head.next.block.hash)  # OUTPUT: hash value for 2nd element
