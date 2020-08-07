da37ff008dff996cb9d06fd1b36a8eb22b76a6f7
Vikalp <vikalp.mishra@gmail.com>
2020-08-07 11:50:49 -0700

    Update problem_2_finding_files.py

--- problem_2_finding_files.py
+++ problem_2_finding_files.py
@@ -35,20 +35,38 @@ def find_files(suffix, path):
     path_list = list()
 
     if not bool(path) or not bool(suffix):
-        print("ERROR: no path or suffix specified")
+        # print("ERROR: no path or suffix specified")
         return path_list
 
+    # check if path has suffix, if yes add it to pth_list
+    if (not os.path.isdir(path)):
+        filename, file_extension = os.path.splitext(path)
+        if file_extension != '':
+            path_list.append(path)
+            return path_list
+
+    
     path_list = find_files_subdir(suffix, path, path_list)
 
     return path_list
 
 
 if __name__=="__main__":
-    # no data test
-    # find_files("", "")
-    # find_files(".c", "")
-    # find_files(".c", None)
-    # find_files(None, "./testdir")
+    # test-1
+    print("------------- test set - 1: no or incorrect data cases")
+    print("Pass" if find_files("", "")==[] else "FAIL")
+    print("Pass" if find_files(".c", "")==[] else "FAIL")
+    print("Pass" if find_files(".c", None)==[] else "FAIL")
+    print("Pass" if find_files(".c", False)==[] else "FAIL")
+    print("Pass" if find_files(None, "./testdir")==[] else "FAIL")
+    print("Pass" if find_files(".md", '')==[] else "FAIL")
+    print("Pass" if find_files(".h", '')==[] else "FAIL")
 
-    # dir-test case
-    print(find_files(".c", "./testdir"))
+    output_1 = ['./testdir/subdir5/a.h']    
+    print("Pass" if find_files(".h", './testdir/subdir5/a.h')==output_1 else "FAIL")
+
+    print("------------- test set - 2: known data cases")
+    output_2 = ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c', './testdir/subdir1/a.c']
+    print("Pass" if find_files(".c", "./testdir")==output_2 else "FAIL")
+
+
